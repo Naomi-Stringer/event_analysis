e4992b0a85426aab309abc7045cd5eb790824bfb
Nick <n.gorman305@gmail.com>
2018-12-11 17:02:45 +1100

    load page formated

--- der_explorer.py
+++ der_explorer.py
@@ -1,20 +1,32 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.dropdown import DropDown
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.uix.filechooser import FileChooserListView
 from kivy.properties import BooleanProperty, ListProperty, StringProperty, ObjectProperty, NumericProperty
 from kivy.uix.popup import Popup
 import os
+import solar_analytics
+from time import time
+import pickle
 
 
 class LoadDialog(FloatLayout):
     load = ObjectProperty(None)
     cancel = ObjectProperty(None)
+    start_path = ObjectProperty(None)
 
     def set_write_id(self, write_2_id):
         self.write_2_id = write_2_id
+
+
+class SaveDialog(FloatLayout):
+    save = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+    text_input = ObjectProperty(None)
 
 
 
@@ -29,17 +41,23 @@ class SubSetLoader(GridLayout):
     def add_data_source(self):
         self.row_count += 1
         self.add_widget(Row(id=str(self.row_count)), index=1)
+        self.height = self.row_count * 30 + 40
 
     def remove_data_source(self, id_2_remove):
         for child in self.children:
             if child.id == id_2_remove:
                 self.remove_widget(child)
+        self.row_count -= 1
+        self.height = self.row_count * 30 + 40
 
     def dismiss_popup(self):
         self._popup.dismiss()
 
-    def choose_file(self, write_2_id):
-        content = LoadDialog(load=self.write_file_name, cancel=self.dismiss_popup)
+    def choose_file(self, write_2_id, row):
+        start_path = row.ids['text_box'].text
+        if start_path == '':
+            start_path = os.getcwd()
+        content = LoadDialog(load=self.write_file_name, cancel=self.dismiss_popup, start_path=start_path)
         content.set_write_id(write_2_id)
         self._popup = Popup(title="Load file", content=content, size_hint=(0.9, 0.9))
         self._popup.open()
@@ -50,13 +68,64 @@ class SubSetLoader(GridLayout):
                 child.ids['text_box'].text = os.path.join(file[0])
         self._popup.dismiss()
 
-class DataLoader(ScrollView):
-    pass
+
+class ExplorerPanel(TabbedPanel):
+    def __init__(self):
+        super(ExplorerPanel, self).__init__()
+        self.ids['time_series'].add_data_source()
+        self.ids['meta_data'].add_data_source()
+        self.ids['inverter_data'].add_data_source()
+
+    def load_data(self):
+        t0 = time()
+        time_interval, time_series, meta_data, inverter_data = self.get_state()
+        data = solar_analytics.get_data_using_file_path_der_explorer(time_interval=time_interval,
+                                                                     data_file_path=time_series,
+                                                                     meta_data_file_path=meta_data,
+                                                                     inverter_data_path=inverter_data)
+        print(data)
+        print('time to load {}'.format(time() - t0))
+
+    def get_state(self):
+        time_interval = None
+        for name, value in self.ids.items():
+            if name in ['s5', 's30', 's60']:
+                if value.active:
+                    time_interval = int(name[1:])
+        for child in self.ids['time_series'].children:
+            if child.id == '1':
+                time_series = child.ids['text_box'].text
+        for child in self.ids['meta_data'].children:
+            if child.id == '1':
+                meta_data = child.ids['text_box'].text
+        for child in self.ids['inverter_data'].children:
+            if child.id == '1':
+                inverter_data = child.ids['text_box'].text
+        return time_interval, time_series, meta_data, inverter_data
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+    def open_save_dialog(self):
+        start_path = os.getcwd()
+        content = SaveDialog(save=self.save_state, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Save data settings", content=content, size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def save_state(self, path, name):
+        path_name = path + '\\' + name
+        state = {}
+        state['time interval'], state['time series'], state['meta data'], state['inverter data']\
+            = self.get_state()
+        if path_name[-2:] != ".p":
+            pickle.dump(state, open(path_name + ".p", "wb"))
+        else:
+            pickle.dump(state, open(path_name, "wb"))
 
 
 class DerExplorerApp(App):
     def build(self):
-        return DataLoader()
+        return ExplorerPanel()
 
 
 if __name__ == "__main__":
--- solar_analytics.py
+++ solar_analytics.py
@@ -135,13 +135,73 @@ def get_data_using_file_path(region_name_string, time_interval, data_file_path, 
 # print(data)
 
 
-
-
-
-
 # --------------------------------------------------25 August data transfer - different format
-def get_august_data_using_file_path(time_interval, data_file_path, meta_data_file_path, inverter_data_path):
+def get_data_using_file_path_der_explorer(time_interval, data_file_path, meta_data_file_path, inverter_data_path):
     # region_name_string = 'not required' 
+    # time_interval = 5 #or 60 or 30
+    # data_file_path = '2018-09-03_solar_analytics_data_transfer/2018-08-25_sa_qld_naomi.csv'
+    # meta_data_file_path = '2018-09-03_solar_analytics_data_transfer/circuit_details.csv'
+    # inverter_data_path = '2018-09-03_solar_analytics_data_transfer/site_details.csv'
+
+    # Get time series data
+    time_series_data = pd.read_csv(data_file_path, index_col='ts', parse_dates=True)
+
+    # Get meta data
+    site_data = pd.read_csv(meta_data_file_path)
+
+    # Combine time series and meta data
+    time_series_data = time_series_data.reset_index().merge(site_data, how='left', on='c_id').set_index('ts')
+
+    # Convert to NEM time (Brisbane time, i.e. no daylight savings). First make aware, then conver time zones, then convert back to niave.
+    time_series_data.index = time_series_data.index.tz_localize('UTC')
+    time_series_data.index = time_series_data.index.tz_convert('Australia/Brisbane').tz_localize(None)
+    # See list of time zones: https://stackoverflow.com/questions/13866926/python-pytz-list-of-timezones
+
+    # Filter for duration specified (i.e. time_interval). Change NaNs to 5s then just filter directly
+    time_series_data.loc[time_series_data.d.isnull(), 'd'] = 5
+    time_series_data = time_series_data[time_series_data['d'] == time_interval]
+
+    # Correct polarity of energy and power
+    time_series_data['p_polarity'] = time_series_data['p'] * time_series_data['polarity']
+    time_series_data['e_polarity'] = time_series_data['e'] * time_series_data['polarity']
+
+    # Add inverter info
+    # Get meta data
+    inverter_data = pd.read_csv(inverter_data_path)
+
+    # Flag sites with multiple inverters then keep only the first instance
+    inverter_model_count = pd.DataFrame({'inverter_model_count' : inverter_data.groupby('site_id')['model'].count()}).reset_index()
+    # Find sites with multiple inverters and add to list
+    multiple_inverter_sites = inverter_model_count[inverter_model_count['inverter_model_count'] > 1.0]
+    multiple_inverter_sites_list = multiple_inverter_sites['site_id'].tolist()
+
+    # Create new column for flagging sites with multiple inverters and flag using list from above
+    inverter_data['multiple_inverter_models'] = np.NaN
+    inverter_data.loc[inverter_data.site_id.isin(multiple_inverter_sites_list), 'multiple_inverter_models'] = 1
+
+    # Drop sites with multiple inverters - since this list doesn't contain c_ids, can just drop duplicates on site_id
+    inverter_data_cut = inverter_data.drop_duplicates(subset = 'site_id', keep = 'first')
+
+    # Combine time series and meta data
+    # This should NOT cause any data deletion issues since left merge uses only keys from right frame (time_series_data) and preserves key order. 
+    # Note that the inverter csv has been adjusted so that each site_id is only listed once. As result,  be careful when filtering by inverter type since some sites will have inverters which are not listed. 
+    time_series_data = time_series_data.reset_index().merge(inverter_data_cut, how = 'left', on='site_id').set_index('ts') 
+
+    # Add power in kW col
+    if time_interval == 30:
+        time_series_data['power_kW'] = time_series_data['e_polarity'] * 0.12 / 3600.0
+    elif time_interval == 5:
+        time_series_data['power_kW'] = time_series_data['e_polarity'] * 0.72 / 3600.0
+    elif time_interval == 60:
+        time_series_data['power_kW'] = time_series_data['e_polarity'] * 0.06 / 3600.0
+    else:
+        print('ERROR - did not specify which data set for energy --> power calc')
+
+    return time_series_data
+
+
+def get_august_data_using_file_path(time_interval, data_file_path, meta_data_file_path, inverter_data_path):
+    # region_name_string = 'not required'
     # time_interval = 5 #or 60 or 30
     # data_file_path = '2018-09-03_solar_analytics_data_transfer/2018-08-25_sa_qld_naomi.csv'
     # meta_data_file_path = '2018-09-03_solar_analytics_data_transfer/circuit_details.csv'
@@ -187,9 +247,9 @@ def get_august_data_using_file_path(time_interval, data_file_path, meta_data_fil
     inverter_data_cut = inverter_data.drop_duplicates(subset = 'site_id', keep = 'first')
 
     # Combine time series and meta data
-    # This should NOT cause any data deletion issues since left merge uses only keys from right frame (time_series_data) and preserves key order. 
-    # Note that the inverter csv has been adjusted so that each site_id is only listed once. As result,  be careful when filtering by inverter type since some sites will have inverters which are not listed. 
-    time_series_data = time_series_data.reset_index().merge(inverter_data_cut, how = 'left', on='site_id').set_index('ts') 
+    # This should NOT cause any data deletion issues since left merge uses only keys from right frame (time_series_data) and preserves key order.
+    # Note that the inverter csv has been adjusted so that each site_id is only listed once. As result,  be careful when filtering by inverter type since some sites will have inverters which are not listed.
+    time_series_data = time_series_data.reset_index().merge(inverter_data_cut, how = 'left', on='site_id').set_index('ts')
 
     # Add power in kW col
     if time_interval == 30:
@@ -202,6 +262,3 @@ def get_august_data_using_file_path(time_interval, data_file_path, meta_data_fil
         print('ERROR - did not specify which data set for energy --> power calc')
 
     return time_series_data
-
-
-
